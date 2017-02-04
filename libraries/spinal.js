c6c44be32fb2c6518678404f1b4a578303c3557c
Jakob Penca <jpenca@me.com>
2017-02-04 14:25:46 +0100

    fixed baseURL in spinal post method

--- libraries/spinal.js
+++ libraries/spinal.js
@@ -3,6 +3,7 @@ function Spinal(baseURL)
     if(baseURL == null)
     {
         baseURL = 'http://127.0.0.1:5000';
+        console.log('no url given, using default: ' + baseURL);
     }
 
     this.baseURL     =  baseURL;
@@ -19,7 +20,7 @@ function Spinal(baseURL)
     this.post        =   function (obj, callback)
                         {
                             $.ajax({
-                                url:        baseURL + '/data',
+                                url:        this.baseURL + '/data',
                                 type:       'post',
                                 dataType:   'json',
                                 data:       JSON.stringify(obj), 
